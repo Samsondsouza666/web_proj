5fd11d40e0dc69a54409f31d361438e65521a979
Samson dsouza <samsondsouza666@gmail.com>
2022-08-12 21:50:52 +0530

    added new card function and modified the code

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -1,29 +1,38 @@
-let firstCard=10;
-let secondCard=1;
+let firstCard=getRandomCard();
+let secondCard=getRandomCard();
+let count=0;
+let card=[firstCard,secondCard];
 let message=" ";
 let isAlive=true;
 let hasBlackJack=false;
 let sum=firstCard+secondCard;
 let messageEl=document.getElementById("message-el");
-
-
-
-
-
-
-
-function startgame()
+let sumEl=document.getElementById("sumEl");
+let cardEL=document.querySelector("#cards-El");
+function getRandomCard()
 {
+    return 5;
+}
+function startGame()
+{
+    renderGame();
+}
+function renderGame()
+{
+    cardEL.textContent ="cards : ";
+    for(let i=0;i<card.length;i++)
+    {
+         cardEL.textContent +=card[i]+" ";
+    }
    if(sum>21)
    {
     message="you're out of the game";
-    isAlive=false;
-   
+    isAlive=false;   
    }
    else if(sum<21)
    {
     message="do you want to draw another card";
-   
+        
 
    }
    else if(sum===21)
@@ -33,6 +42,17 @@ function startgame()
     hasBlackJack=true;
    }
    messageEl.textContent=message;
+   console.log(message);
+   sumEl.textContent ="sum:"+sum;
+}
 
-
+function newCard()
+{
+    if(count==0)
+    {
+     card[2] =getRandomCard();
+    sum+=card[2];
+    renderGame();
+    count++;
+}
 }
